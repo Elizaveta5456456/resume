428a3b68a2853fcefd6f6a12e4d369051eb73b22
Elizaveta5456456 <lizo4ka3007@yandex.ru>
2025-06-13 18:33:42 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Анимация появления элементов при прокрутке
 document.addEventListener('DOMContentLoaded', () => {
     const observer = new IntersectionObserver((entries) => {
         entries.forEach(entry => {
